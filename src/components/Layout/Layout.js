17df3a30530248b2687951080fdea30546350574
Nadia Khoptiuk <holineinadia@gmail.com>
2022-10-20 17:06:35 +0300

    add context, search functionality and edit collections /add en

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
-
 import Header from '../Header';
 import Footer from '../Footer';
+import Search from '../Search';
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Header />
+      <Search />
       <main>{children}</main>
 
       <Footer />
