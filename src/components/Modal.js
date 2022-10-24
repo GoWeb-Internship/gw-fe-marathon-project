83e17f230ad9849559aef37288cd7ae18553909a
Maryna Ostapliuk <marynaum01@gmail.com>
2022-10-24 17:07:56 +0300

    added component no answer in search

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -30,27 +30,8 @@ const Modal = ({ isOpen, closeModal, onNavigate }) => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Panel className="w-full max-w-xl transform overflow-hidden rounded-2xl bg-white p-6 text-center align-middle shadow-xl transition-all">
                   <Search onNavigate={onNavigate} closeModal={closeModal} />
-                  {/* <input
-                    className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm "
-                    placeholder="Search for anything..."
-                    type="text"
-                    name="search"
-                  /> */}
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500"></p>
-                  </div>
-
-                  <div className="mt-4">
-                    <button
-                      type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={closeModal}
-                    >
-                      Close modal
-                    </button>
-                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { Link } from 'gatsby';
+import { useTranslation } from 'gatsby-plugin-react-i18next';
 import Markdown from 'markdown-to-jsx';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { useSearch } from '../../utils/searchContext';
 import qs from 'qs';
+import { useSearch } from '../../utils/searchContext';
 
 const Search = ({ onNavigate, closeModal }) => {
   const [searchPhrase, setSearchPhrase] = useState('');
   const [arrayOfQuestions, setArrayOfQuestions] = useState(null);
   const [filteredQuestions, setFilteredQuestions] = useState(null);
   const { days } = useSearch();
+
+  const { t } = useTranslation();
+  const noAnswer = t('noAnswer', { returnObjects: true });
 
   useEffect(() => {
     if (!days) return;
@@ -59,38 +63,52 @@ const Search = ({ onNavigate, closeModal }) => {
     closeModal();
   };
 
+  console.log(filteredQuestions);
+
   return (
     <div>
       <form>
         <input
-          className="border border-black"
+          className="border-b-2 placeholder:text-slate-400 focus:outline-none w-80 focus:border-sky-500  pt-2 pb-2 pl-3"
           type="text"
           onChange={handleInputChange}
           value={searchPhrase}
+          placeholder={t('input')}
         />
       </form>
 
       {filteredQuestions ? (
-        <ul>
-          {filteredQuestions?.map(({ question_title, chapter, id }) => {
-            return (
-              <li
-                key={id}
-                onClick={() => {
-                  handleRedirect(chapter, id);
-                }}
-                style={{
-                  textOverflow: 'ellipsis',
-                  overflow: 'hidden',
-                  whiteSpace: 'nowrap',
-                }}
-              >
-                <Markdown>{question_title}</Markdown>
-              </li>
-            );
-          })}
-        </ul>
+        <div>
+          <ul>
+            {filteredQuestions?.map(({ question_title, chapter, id }) => {
+              return (
+                <li
+                  key={id}
+                  onClick={() => {
+                    handleRedirect(chapter, id);
+                  }}
+                  className="text-left truncate cursor-pointer hover:text-blue-900"
+                >
+                  <Markdown>{question_title}</Markdown>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       ) : null}
+
+      {filteredQuestions?.length === 0 && (
+        <div className="flex flex-col items-center mt-4">
+          <h3 className="text-slate-900 font-bold text-xl leading-6 w-80 ">
+            {noAnswer.title}{' '}
+            <span className="text-blue-500">{searchPhrase}</span>
+          </h3>
+          <p className="mt-5 text-base">{noAnswer.description}</p>
+          <button className="bg-blue-700 rounded border-2 duration-300 mt-4 border-blue-700 text-white py-4 px-8 hover:text-blue-700 hover:bg-white">
+            <Link to="/feedback">{noAnswer.button}</Link>
+          </button>
+        </div>
+      )}
     </div>
   );
 };
