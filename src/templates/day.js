ffa7691dacbb84489d569efd9b8e81f6909b1cbf
Maryna Ostapliuk <marynaum01@gmail.com>
2022-11-03 22:11:35 +0200

    templete day

--- src/templates/day.js
+++ src/templates/day.js
@@ -1,42 +1,115 @@
 import { withLayout } from '../components/Layout/Layout';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Section from '../components/Section';
-// import { graphql } from 'gatsby';
+import { graphql } from 'gatsby';
 import { useTranslation } from 'react-i18next';
 import ChapterList from '../components/Chapter/ChapterList';
+import Accordion from '../components/Accordion/Accordion';
+import qs from 'qs';
 
-const Day = ({ pageContext }) => {
-  const day = pageContext;
+const Day = ({ data, pageContext, location }) => {
+  console.log(location);
+  console.log(data);
+  const datas = pageContext;
+  const { chapter } = pageContext;
   const { i18n } = useTranslation();
+  console.log(datas);
 
-  console.log(day);
+  const { page: chapters, title: id } = qs.parse(location.search.slice(1));
+  const [questionId, setQuestionId] = useState({});
+
+  const chapterOfPage = chapter.chapter;
+  let objForAccordion = {};
+
+  chapter.subhead.map(element =>
+    element.questions.map(el => (objForAccordion[String(el.id)] = false)),
+  );
+
+  const handleChangeAccordion = id => {
+    setQuestionId(prev => Object.assign({}, prev, { [id]: !prev[id] }));
+  };
+
+  useEffect(() => {
+    if (id) activateCurrentAccordion(questionId, id);
+  }, [id]);
+
+  function activateCurrentAccordion(obj, id) {
+    if (Object.keys(obj).length > 0) {
+      for (let key in obj) {
+        obj[key] = false;
+      }
+
+      obj[id] = true;
+
+      setQuestionId(obj);
+    }
+  }
 
   return (
-    // <Layout>
     <Section styles="main-section">
       <ChapterList />
-      <div className="flex h-screen flex-col items-center justify-center">
-        <h1 className="font-montserrat text-4xl font-bold text-gray-700">
-          День 1
-        </h1>
+      <div>
+        <ul className="subhead-list" id="subhead-list">
+          {data
+            ? data.allMarkdownRemark.nodes.map(({ frontmatter }) => {
+                return frontmatter.subhead.map(
+                  ({ subhead_title, questions }, index) => {
+                    return (
+                      <Accordion
+                        key={index}
+                        subhead_title={subhead_title}
+                        questions={questions}
+                        questionId={questionId}
+                        changeId={handleChangeAccordion}
+                        location={location}
+                        chapter={chapterOfPage}
+                      />
+                    );
+                  },
+                );
+              })
+            : null}
+        </ul>
       </div>
     </Section>
-    // {/* </Layout> */}
   );
 };
 
 export default withLayout(Day);
 
-// export const query = graphql`
-//   query ($language: String!) {
-//     locales: allLocale(filter: { language: { eq: $language } }) {
-//       edges {
-//         node {
-//           ns
-//           data
-//           language
-//         }
-//       }
-//     }
-//   }
-// `;
+export const query = graphql`
+  query ($language: String!) {
+    allMarkdownRemark(
+      filter: {
+        frontmatter: { language: { eq: $language }, chapter: { ne: "start" } }
+      }
+    ) {
+      nodes {
+        frontmatter {
+          title
+          chapter_range
+          chapter
+          language
+          subhead {
+            subhead_title
+            questions {
+              id
+              question_range
+              content: description
+              title: question_title
+            }
+          }
+        }
+      }
+    }
+    locales: allLocale(filter: { language: { eq: $language } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
