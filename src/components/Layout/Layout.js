fb055cbac4c8924d5ab61839e77b36a0dfb8c117
Maryna Ostapliuk <marynaum01@gmail.com>
2022-11-08 09:31:50 +0200

    fix hero height, modal container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
-// import { BrowserRouter } from 'react-router-dom';
-// import { QueryParamProvider } from 'use-query-params';
-// import { ReactRouter6Adapter } from 'use-query-params/adapters/react-router-6';
+import { useState, useEffect } from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer';
 import Hero from '../Hero';
@@ -16,6 +13,12 @@ import {
 const Layout = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
 
+  useEffect(() => {
+    isOpen
+      ? (document.body.style.overflowY = 'hidden')
+      : (document.body.style.overflowY = 'auto');
+  }, [isOpen]);
+
   const closeModal = () => {
     setIsOpen(false);
   };
@@ -24,8 +27,6 @@ const Layout = ({ children }) => {
     setIsOpen(true);
   };
   return (
-    // <BrowserRouter>
-    //   <QueryParamProvider adapter={ReactRouter6Adapter}>
     <div className={layoutWrapper}>
       <div className={`${heroAndHeaderWrapper} dark:bg-accent-dark `}>
         <Header openModal={openModal} />
@@ -37,8 +38,6 @@ const Layout = ({ children }) => {
       <Footer />
       <Modal isOpen={isOpen} closeModal={closeModal} />
     </div>
-    //   </QueryParamProvider>
-    // </BrowserRouter>
   );
 };
 
