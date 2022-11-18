d41752a03f2a7ca532ba8089dcb2409586fc6cfa
Nadia Khoptiuk <holineinadia@gmail.com>
2022-11-18 21:08:16 +0200

    try to fix errors with jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
+  mode: 'jit',
+  // purge: [
+  //   './src/pages/**/*.{js,jsx,ts,tsx}',
+  //   './src/components/**/*.{js,jsx,ts,tsx}',
+  // ],
   darkMode: 'class',
   content: [
     './src/pages/**/*.{js,jsx,ts,tsx}',
